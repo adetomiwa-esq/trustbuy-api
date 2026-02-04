97d47b7e6feeea5681731a6bc978eb8f64fc2ebd
Akinola Kehinde <khennyakin@gmail.com>
2026-02-04 11:17:34 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import { configDotenv } from "dotenv";
+import express from "express";
+import productRoutes from "./src/routes/ProductRoute.js";
+import connectDB from "./src/config/db.js";
+
+configDotenv();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", productRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`listening on PORT: ${process.env.PORT}`);
+});
