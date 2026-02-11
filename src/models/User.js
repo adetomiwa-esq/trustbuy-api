05fb66e3fcd86ab5839ad028fbbb7f965d0857f5
Akinola Kehinde <khennyakin@gmail.com>
2026-02-11 11:09:45 +0100

    feat: create user schema and auth controller

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: false, // VERY IMPORTANT
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
